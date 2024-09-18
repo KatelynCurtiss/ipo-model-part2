bbe0f886456c5a99850943a483f8bfd46ed21770
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-09-18 10:55:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Katelyn Curtiss
+# Setember 18 2024
+# IPO Model(Part 2)
+
+#  /\_/\
+# (=^.^=)
+#  (")(")
+
+
+
+# Input
+num1 = float(input("Please enter a number: "))
+num2 = float(input("Please enter another number: "))
+
+
+# Process
+# Practice useing all the Arithmetic Operators!^.^
+total = num1 // num2
+
+
+
+# Output
+print("The sum of the two number is " + str (total) + ".")
